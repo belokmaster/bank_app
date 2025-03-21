ad33f75f63a4beefb21bb0732f44f17c58267ed4
belokmaster <sychev.nickolay1@gmail.com>
2025-03-21 17:46:52 +0300

    add graph unworked pt

--- go.mod
+++ go.mod
@@ -3,28 +3,29 @@ module bank_app
 go 1.23.5
 
 require (
-	github.com/mattn/go-sqlite3 v1.14.24
-	github.com/xuri/excelize/v2 v2.9.0
+	gonum.org/v1/plot v0.15.2
+	modernc.org/sqlite v1.36.0
 )
 
 require (
+	codeberg.org/go-fonts/liberation v0.4.1 // indirect
+	codeberg.org/go-latex/latex v0.0.1 // indirect
+	codeberg.org/go-pdf/fpdf v0.10.0 // indirect
+	git.sr.ht/~sbinet/gg v0.6.0 // indirect
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/campoy/embedmd v1.0.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
-	github.com/richardlehane/mscfb v1.0.4 // indirect
-	github.com/richardlehane/msoleps v1.0.4 // indirect
-	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
-	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
-	golang.org/x/crypto v0.28.0 // indirect
-	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0 // indirect
-	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/exp v0.0.0-20241009180824-f66d83c29e7c // indirect
+	golang.org/x/image v0.24.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
 	modernc.org/libc v1.61.13 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.8.2 // indirect
-	modernc.org/sqlite v1.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"net/http"
 	"strconv"
 
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/plotutil"
+	"gonum.org/v1/plot/vg"
 	_ "modernc.org/sqlite"
 )
 
@@ -21,6 +25,43 @@ func getCurrentBalance(db *sql.DB) (float64, error) {
 		return 0, nil
 	}
 	return balance, err
+}
+
+func createProfitLossPlot(history []map[string]interface{}) (*plot.Plot, error) {
+	p := plot.New()
+
+	p.Title.Text = "Прибыль и расходы"
+	p.X.Label.Text = "Операции"
+	p.Y.Label.Text = "Сумма"
+
+	profitPoints := make(plotter.XYs, 0)
+	lossPoints := make(plotter.XYs, 0)
+
+	for i, entry := range history {
+		if entry["type"].(string) == "income" {
+			profitPoints = append(profitPoints, plotter.XY{X: float64(i), Y: entry["amount"].(float64)})
+		} else if entry["type"].(string) == "expense" {
+			lossPoints = append(lossPoints, plotter.XY{X: float64(i), Y: entry["amount"].(float64)})
+		}
+	}
+
+	profitLine, err := plotter.NewLine(profitPoints)
+	if err != nil {
+		return nil, err
+	}
+	profitLine.Color = plotutil.Color(0)
+
+	lossLine, err := plotter.NewLine(lossPoints)
+	if err != nil {
+		return nil, err
+	}
+	lossLine.Color = plotutil.Color(1)
+
+	p.Add(profitLine, lossLine)
+	p.Legend.Add("Прибыль", profitLine)
+	p.Legend.Add("Расходы", lossLine)
+
+	return p, nil
 }
 
 func main() {
@@ -169,6 +210,45 @@ func main() {
 		if err := json.NewEncoder(w).Encode(history); err != nil {
 			log.Printf("Ошибка кодирования JSON: %v", err)
 			http.Error(w, "Ошибка кодирования JSON", http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/graphs", func(w http.ResponseWriter, r *http.Request) {
+		// Получаем историю операций из базы данных
+		history, err := GetTransactionHistory(db)
+		if err != nil {
+			log.Printf("Ошибка получения истории: %v", err)
+			http.Error(w, "Ошибка получения истории операций", http.StatusInternalServerError)
+			return
+		}
+
+		// Логируем данные перед отправкой
+		log.Printf("Отправляемые данные: %+v", history)
+
+		// Создаем график на основе истории
+		p, err := createProfitLossPlot(history)
+		if err != nil {
+			log.Printf("Ошибка создания графика: %v", err)
+			http.Error(w, "Ошибка создания графика", http.StatusInternalServerError)
+			return
+		}
+
+		// Устанавливаем заголовок для ответа в формате PNG
+		w.Header().Set("Content-Type", "image/png")
+
+		// Создаем PNG-изображение и отправляем его в ответ
+		writer, err := p.WriterTo(4*vg.Inch, 4*vg.Inch, "png")
+		if err != nil {
+			log.Printf("Ошибка создания изображения: %v", err)
+			http.Error(w, "Ошибка создания изображения", http.StatusInternalServerError)
+			return
+		}
+
+		_, err = writer.WriteTo(w)
+		if err != nil {
+			log.Printf("Ошибка записи изображения: %v", err)
+			http.Error(w, "Ошибка записи изображения", http.StatusInternalServerError)
+			return
 		}
 	})
 
